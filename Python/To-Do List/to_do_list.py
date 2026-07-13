c6aabd4a5200df1d3211f1fcd066ec136e688514
CHIRUMALLA CHARAN ADITYA <charan.bca.sdc@gmail.com>
2026-07-14 00:21:56 +0530

    Add files via upload

--- Python/To-Do List/to_do_list.py
+++ Python/To-Do List/to_do_list.py
@@ -1,1 +1,44 @@
+import tkinter as tk
 
+tasks=[]
+
+def add_task():
+    task=task_entry.get()
+    tasks.append({
+        "task": task,
+        "completed": False
+        })
+    task_entry.delete(0,tk.END)
+    refresh_listbox()
+    
+def refresh_listbox():
+    task_listbox.delete(0, tk.END)
+    for task in tasks:
+        if task["completed"]:
+            display_text = "☑ " + task["task"]
+        else:
+            display_text = "☐ " + task["task"]
+        task_listbox.insert(tk.END, display_text)
+
+
+
+
+
+window=tk.Tk()
+window.title("To Do List")
+window.geometry("500x600")
+window.resizable(False, False)
+
+task_label=tk.Label(window,text="Task")
+task_label.pack()
+
+task_entry = tk.Entry(window)
+task_entry.pack()
+
+AddBtn=tk.Button(window,text="Add Task",command=add_task)
+AddBtn.pack()
+
+task_listbox=tk.Listbox(window,height=10)
+task_listbox.pack()
+
+window.mainloop()
